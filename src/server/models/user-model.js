080004d264193fdbbeaed807e46656d007350a37
Denis Nazarenko <den.nazarenko@icloud.com>
2019-11-17 10:55:21 +0300

    hapi/joi added, user validation

--- src/server/models/user-model.js
+++ src/server/models/user-model.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+const Joi = require('@hapi/joi');
+
 mongoose.set('useCreateIndex', true);
 const userSchema = mongoose.Schema({
   company: String,
@@ -8,6 +10,18 @@ const userSchema = mongoose.Schema({
   userType: String
 });
 userSchema.set('timestamps', true);
+
+userSchema.methods.JoiValidate = (obj) => {
+  const schema = Joi.object({
+    company: Joi.string(),
+    firstName: Joi.string(),
+    lastName: Joi.string(),
+    userType: Joi.string()
+  });
+
+  return Joi.validate(obj, schema);
+};
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
